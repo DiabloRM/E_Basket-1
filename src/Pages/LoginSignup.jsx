d0ecac8fba1ddf1e3f689690415871dda67210f1
Aryan Khanal <khanal.aryan03@gmail.com>
2023-12-06 19:34:49 +0545

    Login page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import './CSS/LoginSignup.css'
+import React, { useState } from 'react';
+import './CSS/LoginSignup.css';
 
 export const LoginSignup = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  const toggleForm = () => {
+    setIsSignUp(!isSignUp);
+  };
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up </h1>
+        <h1>{isSignUp ? 'Sign Up' : 'Login'}</h1>
         <div className='loginsignup-fields'>
-          <input type="text" placeholder='Your Name'/>
-          <input type="email" placeholder='Email Address'/>
-          <input type="password" placeholder='Password'/>
+          {isSignUp && <input type="text" placeholder='Your Name' />}
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account?<span>Login Here</span></p>
-        <div className="loginsignup-agree">
-           <input type="checkbox" name='' id='' />
-           <p>By continuing, I agree to all the terms and condition of use & privacy policy.  </p>
-
-        </div>
+        <button>{isSignUp ? 'Continue' : 'Login'}</button>
+        <p className="loginsignup-login">
+          {isSignUp ? 'Already have an account?' : 'Don\'t have an account?'}
+          <span onClick={toggleForm}>{isSignUp ? 'Login Here' : 'Sign Up Here'}</span>
+        </p>
+        {isSignUp && (
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>By continuing, I agree to all the terms and conditions of use & privacy policy.</p>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
+
 export default LoginSignup;

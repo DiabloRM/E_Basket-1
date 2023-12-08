687bc03435a68d77d2faf72511539c46f65cb233
gchat <gchat@LAPTOP-N8ORO8D3>
2023-12-08 22:11:41 +0545

    login signupnfunctional

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
-import './Navbar.css'
-import logo from '../Assets/logo1.png'
-import cart_icon from '../Assets/cart_icon.png'
+import './Navbar.css';
 import { Link } from 'react-router-dom';
+import logo from '../Assets/logo1.png';
+import cart_icon from '../Assets/cart_icon.png';
+import search_icon from '../Assets/search_icon.png';
 
 const Navbar = () => {
-    const [menu,setMenu] = useState("shop");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    // Add your search logic here
+    console.log("Search query:", searchQuery);
+  };
+
   return (
     <div className='navbar'>
-        <div className="nav-logo">
-            <img src={logo} alt = "" />
-         <p>- BASKET</p>
-        </div>
-        <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}><Link to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className='nav-login-cart'>
-            <Link to='/login'><button>Login</button></Link>
-            <Link to='/cart'><img src={cart_icon} alt=''></img></Link>
-            <div className='nav-cart-count'>0</div>
-        </div>
-        
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>- BASKET</p>
+      </div>
+      <div className='nav-search'>
+        <input
+          type='text'
+          placeholder='Search products...'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>
+          <img src={search_icon} alt='Search' />
+        </button>
+      </div>
+      <div className='nav-login-cart'>
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt='' /></Link>
+        <div className='nav-cart-count'>0</div>
+      </div>
     </div>
   );
 };

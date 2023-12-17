162410feb22fed81d3e14780cec2db5c2c1cf5ee
Aryan Khanal <khanal.aryan03@gmail.com>
2023-12-17 12:20:43 +0545

    SearchResults update and other changes

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -1,35 +1,30 @@
-// SearchResults.js
-
 import { useLocation } from 'react-router-dom';
-import { searchProducts } from '../Components/Assets/all_product';
+import { all_product } from '../Components/Assets/all_product';
 import './CSS/SearchResults.css';
+import Item from "../Components/Item/Item";
 
 const SearchResults = () => {
   const location = useLocation();
-  const searchQuery = new URLSearchParams(location.search).get("q");
-  const results = searchProducts(searchQuery);
+  const searchQuery = new URLSearchParams(location.search).get('q');
+  const results = all_product.filter(item => item.name.toLowerCase().includes(searchQuery.toLowerCase()));
 
   return (
     <div className="search-results-container">
       <h2 className="search-results-subheader">Search Results for "{searchQuery}"</h2>
-      <h3> </h3> 
-      <br /> 
-      <h3> </h3> 
-      {results.map((product) => (
-        <div key={product.id}>
-          <a
-            href={`#product/${product.id}`} // Add your product link or route here
-            className="search-result-item">
-            <img className="product-image" src={product.image} alt={product.name} />
-            <p className="product-name">{product.name}</p>
-            <p className="product-category">{product.category}</p>
-            <div className="product-prices">
-            <p className="old-price">${product.old_price}</p>
-            <p>${product.new_price}</p>
-            </div>
-            {/* Add more details as needed */}
-          </a>
-          <br />
+      <div></div>
+      <div></div>
+      <div></div>
+      {results.map((item, idx) => (
+        <div className="search-result-item" key={idx}>
+          {item.category && (
+            <Item
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          )}
         </div>
       ))}
     </div>
--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,5 +1,4 @@
-// UserPage.js
-
+/*
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, firestore } from '../firebase';
@@ -8,6 +7,7 @@ import './CSS/UserPage.css';
 const UserPage = () => {
   const [user] = useAuthState(auth);
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -17,36 +17,41 @@ const UserPage = () => {
           setUserData(userDoc.data());
         }
       } catch (error) {
-        console.error('Error fetching user data:', error.message);
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false);
       }
     };
-
+  
     if (user) {
       fetchUserData();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, firestore]);
+  
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   // Function to get initials from the first and last name
-  const getInitials = (firstName, lastName) => {
-    return `${firstName.charAt(0)}${lastName.charAt(0)}`;
+  const getInitials = (firstname, lastname) => {
+    return `${firstname ? firstname.charAt(0) : ''}${lastname ? lastname.charAt(0) : ''}`;
   };
-
   return (
     <div className="user-page-container">
       <div className="profile-photo">
         {userData && (
           <>
             <div className="initials">
-              {getInitials(userData.firstName, userData.lastName)}
+              {getInitials(userData.firstname, userData.lastname)}
             </div>
             <div className="name">
-              {userData.firstName} {userData.lastName}
+              {userData.firstname} {userData.lastname}
             </div>
           </>
         )}
       </div>
-      <h1>Welcome, {userData ? `${userData.firstName} ${userData.lastName}` : user ? 'user' : ''}!</h1>
+      <h1>Welcome, {userData ? `${userData.firstname} ${userData.lastname}` : user ? 'user' : ''}!</h1>
       {user && (
         <div>
           <p>Email: {user.email}</p>
@@ -57,3 +62,83 @@ const UserPage = () => {
 };
 
 export default UserPage;
+*/
+
+
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '../firebase';
+import './CSS/UserPage.css';
+
+const UserPage = () => {
+  const [user] = useAuthState(auth);
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userDoc = await firestore.collection('users').doc(user.uid).get();
+        if (userDoc.exists) {
+          setUserData(userDoc.data());
+        }
+
+        // Assuming 'username' is a field in the user document
+        const usernameDoc = await firestore.collection('users').doc(user.uid).get();
+        if (usernameDoc.exists) {
+          setUserData(prevData => ({
+            ...prevData,
+            username: usernameDoc.data().username,
+          }));
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (user) {
+      fetchUserData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, firestore]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="user-page-container">
+      <div className="profile-photo">
+        {userData && userData.firstname && userData.lastname ? (
+          <>
+            <div className="initials">
+              {`${userData.firstname.charAt(0)}${userData.lastname.charAt(0)}`}
+            </div>
+            <div className="name">
+              {`${userData.firstname} ${userData.lastname}`}
+            </div>
+          </>
+        ) : (
+          <div className="initials">
+            {/* Handle cases where firstname or lastname is missing */}
+            {user ? `${user.email.charAt(0).toUpperCase()}` : ''}
+          </div>
+        )}
+      </div>
+      <h1>
+        Welcome, {userData ? `${userData.firstname} ${userData.lastname}` : user ? `${user.email}` : ''}!
+      </h1>
+      {userData && (
+        <div>
+          <p>Username: {userData.username}</p>
+          <p>Email: {user.email}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserPage;
+
